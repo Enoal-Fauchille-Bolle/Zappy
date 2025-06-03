28ade30f07cb57688fc359db82baf79573747cff
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-06-03 16:01:02 +0200

    fix(server): 🔒️ | enforce port validation range
    corrects port validation to require values between 1024 and 65535. This helps enhance server security by preventing the acceptance of low, commonly reserved ports.

--- src/server/options/port.c
+++ src/server/options/port.c
@@ -28,7 +28,7 @@ void handle_port(server_options_t *options, int *i, int ac, char **av)
     if (*i + 1 < ac) {
         *i += 1;
         options->port = atoi(av[*i]);
-        if (options->port <= 0 || options->port > 65535) {
+        if (options->port < 1024 || options->port > 65535) {
             dprintf(fileno(stderr),
                     "Error: Port must be a number between 1 and 65535\n");
             options->error = true;
