3938dd02646db0e83f0af7a69783ea0dcf2d75d9
Tom Feldkamp <tom.feldkamp@epitech.eu>
2025-06-16 11:25:10 +0200

    refactor(Entity): :recycle: | enhance documentation for constructors, destructors, and methods for improved clarity

--- src/gui/entity/Entity.cpp
+++ src/gui/entity/Entity.cpp
@@ -8,15 +8,31 @@
 #include "Entity.hpp"
 #include <iostream>
 
+/**
+ * @brief Construct a new Entity object.
+ *
+ * @param id The unique identifier for the entity.
+ * @param meshName The name of the mesh to use for the entity.
+ */
 Entity::Entity(const std::string &id, const std::string &meshName)
     : _id(id), _meshName(meshName), _ogreEntity(nullptr), _sceneNode(nullptr), _isAttached(false)
 {
 }
 
+/**
+ * @brief Destroy the Entity object.
+ */
 Entity::~Entity()
 {
 }
 
+/**
+ * @brief Set the position of the entity.
+ *
+ * @param x X coordinate.
+ * @param y Y coordinate.
+ * @param z Z coordinate.
+ */
 void Entity::setPosition(float x, float y, float z)
 {
     if (_sceneNode) {
@@ -24,6 +40,13 @@ void Entity::setPosition(float x, float y, float z)
     }
 }
 
+/**
+ * @brief Set the rotation of the entity.
+ *
+ * @param x Pitch angle in degrees.
+ * @param y Yaw angle in degrees.
+ * @param z Roll angle in degrees.
+ */
 void Entity::setRotation(float x, float y, float z)
 {
     if (_sceneNode) {
@@ -34,6 +57,13 @@ void Entity::setRotation(float x, float y, float z)
     }
 }
 
+/**
+ * @brief Set the scale of the entity.
+ *
+ * @param x Scale in X direction.
+ * @param y Scale in Y direction.
+ * @param z Scale in Z direction.
+ */
 void Entity::setScale(float x, float y, float z)
 {
     if (_sceneNode) {
@@ -41,6 +71,11 @@ void Entity::setScale(float x, float y, float z)
     }
 }
 
+/**
+ * @brief Set the color of the entity.
+ *
+ * @param color The color to set.
+ */
 void Entity::setColor(zappy_color color)
 {
     if (_ogreEntity) {
@@ -52,21 +87,36 @@ void Entity::setColor(zappy_color color)
     }
 }
 
+/**
+ * @brief Get the ID of the entity.
+ *
+ * @return std::string The entity ID.
+ */
 std::string Entity::getId() const
 {
     return _id;
 }
 
+/**
+ * @brief Get the scene node associated with the entity.
+ *
+ * @return Ogre::SceneNode* The scene node pointer.
+ */
 Ogre::SceneNode* Entity::getSceneNode() const
 {
     return _sceneNode;
 }
 
+/**
+ * @brief Attach the entity to the scene.
+ *
+ * @param sceneManager The scene manager to use.
+ */
 void Entity::attachToScene(Ogre::SceneManager* sceneManager)
 {
     if (!_isAttached && sceneManager) {
         try {
-            _ogreEntity = sceneManager->createEntity(_id, "cube.mesh");
+            _ogreEntity = sceneManager->createEntity(_id, _meshName);
             _sceneNode = sceneManager->getRootSceneNode()->createChildSceneNode(_id + "_node");
             _sceneNode->attachObject(_ogreEntity);
             _isAttached = true;
