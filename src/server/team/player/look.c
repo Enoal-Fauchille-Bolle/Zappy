dd9629939061c8a6b3ab38d6f7404350e2c98b7c
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-06-18 11:53:47 +0200

    feat(player): ✨ | implement resource concatenation in get_tile_contents function

--- src/server/team/player/look.c
+++ src/server/team/player/look.c
@@ -6,6 +6,7 @@
 */
 
 #include "map/map.h"
+#include "map/resource_names.h"
 #include "map/tile.h"
 #include "team/player/player.h"
 #include "utils/string.h"
@@ -20,9 +21,24 @@ const pos_t directions[4] = {
     {-1, 0}       // WEST
 };
 
+static void concat_resources(char **contents, tile_t *tile)
+{
+    char *temp;
+
+    for (int i = 0; i <= THYSTAME; i++) {
+        temp = repeat_string(resource_names[i], " ", tile->resources[i]);
+        if ((*contents)[strlen(*contents) - 1] != ' ' &&
+            tile->resources[i] > 0)
+            *contents = dyn_strcat(*contents, " ");
+        *contents = dyn_strcat(*contents, temp);
+        free(temp);
+    }
+}
+
 static char *get_tile_contents(tile_t *tile)
 {
     char *contents = empty_string();
+    char *temp;
 
     if (tile == NULL) {
         fprintf(stderr, "Invalid tile pointer\n");
@@ -32,12 +48,17 @@ static char *get_tile_contents(tile_t *tile)
         perror("Failed to allocate memory for empty string");
         return NULL;
     }
+    temp = repeat_string("player", " ", get_nb_players_on_tile(tile));
+    contents = dyn_strcat(contents, temp);
+    free(temp);
+    concat_resources(&contents, tile);
     return contents;
 }
 
 char *look(player_t *player, map_t *map)
 {
     char *contents = empty_string();
+    char *tile_content;
 
     if (player == NULL || map == NULL) {
         fprintf(stderr, "Invalid player or map pointer\n");
@@ -47,8 +68,12 @@ char *look(player_t *player, map_t *map)
         perror("Failed to allocate memory for contents");
         return NULL;
     }
-    dyn_strcat(contents, "[");
-    if (player->level == 1)
-        dyn_strcat(contents, get_tile_contents(get_tile(map, player->pos)));
+    contents = dyn_strcat(contents, "[");
+    if (player->level == 1) {
+        tile_content = get_tile_contents(get_tile(map, player->pos));
+        contents = dyn_strcat(contents, tile_content);
+        free(tile_content);
+    }
+    contents = dyn_strcat(contents, "]");
     return contents;
 }
