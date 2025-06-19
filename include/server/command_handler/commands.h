84cf9e9b0ae2c495bfc62106007bccf64e18b273
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-06-19 10:03:59 +0200

    feat(commands): ✨ | add command cooldown constants

--- include/server/command_handler/commands.h
+++ include/server/command_handler/commands.h
@@ -12,6 +12,19 @@
     #include "connection/client.h"
     #include <stdbool.h>
 
+    #define FORWARD_COMMAND_COOLDOWN 7
+    #define LEFT_COMMAND_COOLDOWN 7
+    #define RIGHT_COMMAND_COOLDOWN 7
+    #define LOOK_COMMAND_COOLDOWN 7
+    #define INVENTORY_COMMAND_COOLDOWN 1
+    #define BROADCAST_COMMAND_COOLDOWN 7
+    #define CONNECT_NBR_COMMAND_COOLDOWN 0
+    #define FORK_COMMAND_COOLDOWN 42
+    #define EJECT_COMMAND_COOLDOWN 7
+    #define TAKE_COMMAND_COOLDOWN 7
+    #define SET_COMMAND_COOLDOWN 7
+    #define INCANTATION_COMMAND_COOLDOWN 300
+
 void dummy_command(client_t *client, command_t *command);
 
 #endif /* !COMMANDS_H_ */
