d05c3748a2f626dd60b6343e5b32d40422851f58
KyllianChandelier <kyllian.chandelier@epitech.eu>
2025-06-19 15:02:29 +0200

    Feat :sparkles: | add command handler
    the command handler register all the command available, and call the right one upon receiving a message
    
    Start #167

---
+++ src/gui/command/CommandHandler.hpp
@@ -0,0 +1,21 @@
+#ifndef COMMAND_HANDLER_HPP
+#define COMMAND_HANDLER_HPP
+
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include "ICommand.hpp"
+#include "../GameManager.hpp"
+
+class CommandHandler {
+public:
+    CommandHandler();
+    ~CommandHandler();
+
+    void handleCommand(const std::string& command, const std::string& args, SimpleGameManager gameManager);
+
+private:
+    std::unordered_map<std::string, std::unique_ptr<ICommand>> _commands;
+};
+
+#endif // COMMAND_HANDLER_HPP
