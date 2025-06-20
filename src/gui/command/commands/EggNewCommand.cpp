2617001bcc100940f6c3718caacf090d8bd36e74
KyllianChandelier <kyllian.chandelier@epitech.eu>
2025-06-20 12:05:12 +0200

    Feat :sparkles: | enw command
    egg spawn at the server's demand
    
    End #128

--- src/gui/command/commands/EggNewCommand.cpp
+++ src/gui/command/commands/EggNewCommand.cpp
@@ -7,12 +7,20 @@
 
 #include "EggNewCommand.hpp"
 #include <iostream>
+#include <sstream>
 
 EggNewCommand::EggNewCommand()
 {
 }
 
 void EggNewCommand::execute(const std::string& args, SimpleGameManager& gameManager) {
-    (void)gameManager;
-    std::cout << "EggNewCommand " << args << std::endl;
+    std::string idStr, parentIdStr;
+    int id, parentId, x, y;
+
+    std::istringstream iss(args);
+    iss >> idStr >> parentIdStr >> x >> y;
+
+    id = std::stoi(idStr.substr(1));
+    parentId = std::stoi(parentIdStr.substr(1));
+    gameManager.createEgg(id, parentId, x, y);
 }
