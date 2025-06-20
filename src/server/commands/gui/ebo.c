ce97fad2c5489b0debb4fcc8a9146bdd6137d213
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-06-20 17:51:03 +0200

    docs(gui): 📝 | add documentation for ebo_command function

--- src/server/commands/gui/ebo.c
+++ src/server/commands/gui/ebo.c
@@ -12,6 +12,15 @@
 #include <stddef.h>
 #include <stdio.h>
 
+/**
+ * @brief Sends the ebo command to all GUI clients
+ *
+ * This function sends the "ebo" command with the egg ID to all GUI clients
+ * connected to the server. It is used to notify GUIs about an egg's existence.
+ *
+ * @param egg Pointer to the egg structure containing the egg ID and team
+ * information. If NULL, the function does nothing.
+ */
 void ebo_command(egg_t *egg)
 {
     if (egg == NULL)
