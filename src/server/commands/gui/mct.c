54b649a080c8940c01b7730d0e0a7ef52525c0d9
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-06-21 18:23:30 +0200

    refactor(gui): ♻️ | extract tile iteration to separate function
    #171

--- src/server/commands/gui/mct.c
+++ src/server/commands/gui/mct.c
@@ -48,6 +48,36 @@ static void send_tile_info(client_t *client, tile_t *tile, pos_t pos)
 }
 
 /**
+ * @brief Iterate through the tiles in the map and send their information to
+ * the client.
+ *
+ * This function retrieves each tile in the map and sends its resource
+ * information to the client using the send_tile_info function.
+ *
+ * @param client The client requesting the tile information.
+ * @param pos The current position in the map being processed.
+ * @param vtable The vector vtable used to access the tiles.
+ */
+static void iterate_tiles(
+    client_t *client, pos_t pos, const vector_vtable_t *vtable)
+{
+    tile_t *tile = vtable->at(client->server->game->map->tiles,
+        pos.y * client->server->game->map->width + pos.x);
+
+    if (tile == NULL) {
+        debug_error(client->server->options->debug,
+            "Failed to retrieve tile at (%d, %d)\n", pos.x, pos.y);
+        return;
+    }
+    send_tile_info(client, tile, pos);
+    pos.x++;
+    if (pos.x >= (int)client->server->game->map->width) {
+        pos.x = 0;
+        pos.y++;
+    }
+}
+
+/**
  * @brief Handle the mct command for the GUI.
  *
  * This function retrieves the tile information at the specified position and
@@ -60,24 +90,11 @@ void mct_command(client_t *client, command_t *command)
 {
     const vector_vtable_t *vtable =
         vector_get_vtable(client->server->game->map->tiles);
-    tile_t *tile = NULL;
     pos_t pos = {0, 0};
 
     (void)command;
     while (pos.y < (int)client->server->game->map->height &&
-           pos.x < (int)client->server->game->map->width) {
-        tile = vtable->at(client->server->game->map->tiles,
-            pos.y * client->server->game->map->width + pos.x);
-        if (tile == NULL) {
-            debug_error(client->server->options->debug,
-                "Failed to retrieve tile at (%d, %d)\n", pos.x, pos.y);
-            return;
-        }
-        send_tile_info(client, tile, pos);
-        pos.x++;
-        if (pos.x >= (int)client->server->game->map->width) {
-            pos.x = 0;
-            pos.y++;
-        }
+        pos.x < (int)client->server->game->map->width) {
+        iterate_tiles(client, pos, vtable);
     }
 }
