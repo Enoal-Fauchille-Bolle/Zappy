58ab8247a58701f34d705d1e3e1c31c1a3545e27
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-06-24 11:16:34 +0200

    refactor(game): 🚚  | move incantation requirements constants

--- include/server/game/game_constants.h
+++ include/server/game/game_constants.h
@@ -30,4 +30,24 @@
     #define SET_COMMAND_COOLDOWN 7
     #define INCANTATION_COMMAND_COOLDOWN 300
 
+static const inventory_t INCANT_REQS[MAX_PLAYER_LEVEL - 1] = {
+    [0] = {0, 1, 0, 0, 0, 0, 0},      // Level 1 to 2
+    [1] = {0, 1, 1, 1, 0, 0, 0},      // Level 2 to 3
+    [2] = {0, 2, 0, 1, 0, 2, 0},      // Level 3 to 4
+    [3] = {0, 1, 1, 2, 0, 1, 0},      // Level 4 to 5
+    [4] = {0, 1, 2, 1, 3, 0, 0},      // Level 5 to 6
+    [5] = {0, 1, 2, 3, 0, 1, 0},      // Level 6 to 7
+    [6] = {0, 2, 2, 2, 2, 2, 1}       // Level 7 to 8
+};
+
+static const size_t NB_PLAYERS_REQUIRED[MAX_PLAYER_LEVEL - 1] = {
+    [0] = 1,      // Level 1 to 2
+    [1] = 2,      // Level 2 to 3
+    [2] = 2,      // Level 3 to 4
+    [3] = 4,      // Level 4 to 5
+    [4] = 4,      // Level 5 to 6
+    [5] = 6,      // Level 6 to 7
+    [6] = 6       // Level 7 to 8
+};
+
 #endif /* !GAME_CONSTANTS_H_ */
