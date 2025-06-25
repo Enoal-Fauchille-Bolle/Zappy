53f3dabbf9ac282300cad34f53d0f207a86a6328
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-06-25 13:23:17 +0200

    refactor(ai): ♻️ | simplify incantation command logic

--- src/server/commands/ai/incantation_cmd.c
+++ src/server/commands/ai/incantation_cmd.c
@@ -64,11 +64,11 @@ void incantation_command(client_t *client, command_t *command)
     if (!can_start_incantation(client))
         return;
     client->player->tick_cooldown = INCANTATION_COMMAND_COOLDOWN;
-    create_incantation(
-        client->server->game, client->player->pos, client->player->level);
     debug_game(client->server->options->debug,
         "Player %zu started an "
         "incantation at (%d, %d) for level %u -> %u\n",
         client->player->id, client->player->pos.x, client->player->pos.y,
         client->player->level, client->player->level + 1);
+    create_incantation(
+        client->server->game, client->player->pos, client->player->level);
 }
